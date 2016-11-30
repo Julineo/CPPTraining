5d88efdfd6f05ff92dd0d0eb73cf201e368e4781
Julineo <advoretskiy80@gmail.com>
2016-11-30 14:55:17 -0800

    Added Merge Sort algorithm

--- 6/PPA5.cpp
+++ 6/PPA5.cpp
@@ -34,7 +34,7 @@ using namespace std;
 float marksarray[][2] : Contains the midsem_marks and endsem_marks of 'N' students 
 int index1            : Denotes the the row number(index) of the student. 
 int index2            : Denotes the the row number(index) of the student.  */
-int comparator(float marksarray[][2],int index1, int index2) 
+int comparator(float marksarray[][2], int index1, int index2) 
 {
     //Write your code below this line to calculate RMS, compare them, and return either -1, 0, or 1  
 	int a1, a2;
@@ -115,12 +115,55 @@ float sortedarray[][2] : Containing the sorted elements (based on the sorting do
 float mergedarray[][2] : After this function executes, this should contain the merged array.
 int break_point        : Denoting the point i.e. the row number as explained earlier.
 int numElements        : Number of rows in both the arrays (sortedarray and mergedarray). */
-int merge(float sortedarray[][2], float mergedarray[][2], int break_point, int numElements) 
+int merge(float sortedarray[][2], float mergedarray[][2], int break_point, int numElements)
 {
     // Write your code below this line to merge non-duplicate array rows of Part A and Part B
     // Return the number of non-duplicate rows detected in the entire array after merging 
-
-    
+	float temparray[numElements][2]; //we can't delete elements from static array in C++, so we'll do some funky stuff
+	int k = 0; 
+	int l = break_point;
+	int s = 0; //index for temparray
+	
+	for (int i = 0; i < numElements; i++)//loop through all elements before break_point")".
+	{
+		if (comparator(sortedarray, k,  l) == 0)//equal
+		{
+			k++;
+			l++;
+			cout << "k =" << k << endl;
+			cout << "l =" << l << endl;
+			continue;
+		}
+		if (comparator(sortedarray, k,  l) == 1)//right is bigger, copy smaller left
+		{
+			temparray[s][0] = sortedarray[k][0];
+			temparray[s][1] = sortedarray[k][1];
+			cout << "sortedarray[k][0]: " << sortedarray[k][0] << endl;
+			k++;
+			s++;
+			cout << "k1 =" << k << endl;
+			cout << "s =" << s << endl;
+			continue;
+		}
+		if (comparator(sortedarray, k,  l) == -1)//left is bigger, copy smaller right
+		{
+			temparray[s][0] = sortedarray[l][0];
+			temparray[s][1] = sortedarray[l][1];
+			cout << "sortedarray[l][0]: " << sortedarray[l][0] << endl;
+			l++;
+			s++;
+			cout << "l1 =" << l << endl;
+			cout << "s =" << l << endl;
+			continue;
+		}
+	}
+	for (int i = 0; i < numElements; i++)//copy one array to another
+	{
+		mergedarray[i][0] = temparray[i][0];
+		mergedarray[i][1] = temparray[i][1];
+	}
+	//mergedarray = temparray;
+	return k;    
 }
 
 
@@ -129,13 +172,21 @@ int main()
 {
     //float marksarray[][2] = {{90,30}, {60,20}, {30,40}, {60,20}, {10,90}, {0,100}, {60,60}, {50,10}, {10, 90}, {0, 100}};
 	//float marksarray[][2] = {{90,30}, {60,20}, {30,40}, {60,20}, {10,90}, {0,100}};
-	float marksarray[][2] = {{60,20}, {60,20}, {30,40}, {10,90}, {90,30},{0,100},{60,20}};
+	//float sortedarray[][2] = {{60,20}, {60,20}, {30,40}, {10,90}, {90,30},{0,100},{60,20}};
+	//float marksarray[][2] = {{60,20}, {60,20}, {30,40}, {10,90}, {90,30},{0,100},{60,20}};
+	float sortedarray[][2] = {{30, 40}, {60, 20}, {60, 20}, {10, 90}, {90, 30}, {0, 100}, {60, 20}};//sorted array, debug
+	float marksarray[][2] = {{30, 40}, {60, 20}, {60, 20}, {10, 90}, {90, 30}, {0, 100}, {60, 20}};//sorted array, debug
 	int n = ARRAY_SIZE(marksarray);
+	
+	cout << comparator(sortedarray, 0,  5) << endl;
 
 
 	//cout << comparator(marksarray,1,2) << endl;//1
-	int break_point = selectionSort2(marksarray, n) << endl;
-	cout << merge(marksarray, marksarray, break_point, n) << endl;
+	int break_point = selectionSort2(sortedarray, n);
+	int break_point2 = selectionSort2(marksarray, n);
+	cout << "sortedarray:" << sortedarray << endl;
+
+	cout << merge(sortedarray, marksarray, break_point, n) << endl;
 	
     return 0;
 }
